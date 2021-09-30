44d14f5e47dbf6482900fed03b879975986c0541
Huzaifa Rabbani <huzaifarabbani@Huzaifas-MacBook-Pro.local>
2021-09-30 18:10:47 -0400

    done with Stall class

--- hw4.py
+++ hw4.py
@@ -27,8 +27,9 @@ class Customer:
     
     # Submit_order takes a cashier, a stall and an amount as parameters, 
     # it deducts the amount from the customer’s wallet and calls the receive_payment method on the cashier object
-    def submit_order(self, cashier, stall, amount): 
-        pass
+    def submit_order(self, cashier, stall, amount):
+		self.wallet -= amount
+        cashier.receive_payment(stall, amount)
 
     # The __str__ method prints the customer's information.    
     def __str__(self):
@@ -71,8 +72,28 @@ class Cashier:
 
 ## Complete the Stall class here following the instructions in HW_4_instructions_rubric
 class Stall:
-    
-    pass
+    def __init__(self, name, inventory, cost = 7, earnings = 0) :
+		self.name = name
+		self.inventory = inventory
+		self.cost = cost
+		self.earnings = earnings
+
+    def process_order(self, food_name, quantity) :
+		if food_name in self.inventory :
+			self.inventory[food_name] -= quantity
+			self.money = quantity * self.cost
+
+    def has_item(self, food_name, quantity) :
+		if food_name in self.inventory :
+			if self.inventory[food_name] > 0 :
+				return True
+		return False
+
+    def stock_up(self, food_name, quantity) :
+		if food_name in self.inventory :
+			self.inventory[food_name] += quantity
+		else:
+			self.inventory[food_name] = quantity
 
 
 class TestAllMethods(unittest.TestCase):
